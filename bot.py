df341cb004fb05de85c1223ffdecbbdb3614d439
Andrey Smirnoff <andrinoff.69@gmail.com>
2024-09-12 10:20:37 +0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, ConversationHandler, MessageHandler, filters
 from rate import convert
+import decimal
 
 
 
@@ -11,79 +12,65 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-
+# fsfsdf
 while True:
- # Start command.
-
-    AMOUNT, CURRENCY1, CURRENCY2, EXCHANGE, START = range(5)
-
-
-
-
+    AMOUNT, CURRENCY1, CURRENCY2, EXCHANGE, ERROR = range(5)
     async def amount(update: Update, context: ContextTypes.DEFAULT_TYPE):
-            global amount1
+
             amount1 = update.message.text
             print(amount1)
+            context.user_data["amount"] = decimal(amount1)
             try:
-                amount1 = int(amount1)
                 await update.message.reply_text('Now write the currency (e.g. usd, gel, eur)')
-                amount1 = str(amount1)
-                return AMOUNT
-            except Exception: 
+                return CURRENCY1
+            except ValueError: 
                 await update.message.reply_text("Error, write just the amount.") 
-                await amount(Update, ContextTypes.DEFAULT_TYPE)
+                await amount(update, context)
                 
 
 
     async def currency1(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        
-        
-        global currency_1
         currency_1 = update.message.text
-
+        context.user_data["currency1"] = currency_1
         currency_1 = currency_1.strip().upper()
         print(currency_1)
         currency_1 = str(currency_1)
         try:
             if len(currency_1) == 3:
-                await update.message.reply_text ('Write the currency you want ' + amount1 + currency_1 + ' in')
-                return CURRENCY1
+                await update.message.reply_text ('Write the currency you want ' + context.user_data["amount"] + context.user_data['currency1'] + ' in')
+                return CURRENCY2
             else:
                 await update.message.reply_text("Error, write just the currency! (It has to be 3 letters long) (e.g USD, GEL)") 
         except Exception:
             await update.message.reply_text("Error, try again through /exchange")
-            await exchange(Update, ContextTypes.DEFAULT_TYPE)
+            await exchange(update, context)
         
         
     async def currency2(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        
-        global currency_2
         currency_2 = update.message.text
-       
         currency_2 = currency_2.strip().upper()
         print(currency_2)
+        context.user_data["currency2"] = currency_2
         currency_2 = str(currency_2)
         try:
             if len(currency_2) == 3:
-                    finalresult = convert(amount1, currency_1, currency_2)
+                    finalresult = convert(context.user_data["amount"], context.user_data["currency1"], context.user_data["currency2"])
                     finalresult = str(finalresult)
-                    await update.message.reply_text('Your rate for ' +amount1+currency_1 + ' = '+ finalresult+ currency_2+ '.\n\n\n\nThank you for using my service! \nType anything to proceed!\n\nCredits: @andrinoff')
-                    return CURRENCY2
+                    await update.message.reply_text('Your rate for ' +context.user_data["amount"]+context.user_data["currency1"] + ' = '+ finalresult+ context.user_data["currency1"]+ '.\n\n\n\nThank you for using my service! \nType anything to proceed!\n\nCredits: @andrinoff')
+                    return EXCHANGE
             else:
                 await update.message.reply_text("Error, write just the currency! (It has to be 3 letters long) (e.g USD, GEL)") 
         except Exception:
             await update.message.reply_text("Error, try again through /exchange")
-            await exchange(Update, ContextTypes.DEFAULT_TYPE)
+            await exchange(update, context)
 
         
-    async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        user = update.message.from_user
-        await context.bot.send_message(chat_id=update.effective_chat.id, 
-                                    text='Hello, ' + user.first_name  + '! \n Use "/exchange" to see exchange \n\n Credits:\n @andrinoff')
-        amount1 = None
-        currency_1 = None
-        currency_2 = None
-        return
+    async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text ('An error encounted, reported \n You will start over')
+        context.user_data['amount'] = None
+        context.user_data['currency1']  = None
+        context.user_data['currency2']  = None
+        return EXCHANGE
     async def exchange(update: Update, context: ContextTypes.DEFAULT_TYPE):
         amount1 = None
         currency_1 = None
@@ -91,38 +78,34 @@ while True:
         await update.message.reply_text('Write AMOUNT (e.g. 20)')
 
                 
-        return EXCHANGE
+        return AMOUNT
 
 
     if __name__ == '__main__':
-        application = ApplicationBuilder().token('7307380567:AAHrnAsxUxwlg7cXWGjFvwlBS_NmoyirJ4I').build()
-
-        start_handler = CommandHandler('start', start)
-        
+        application = ApplicationBuilder().token('7307380567:AAHrnAsxUxwlg7cXWGjFvwlBS_NmoyirJ4I').build()        
         exchange_handler = ConversationHandler(
             entry_points= [CommandHandler('exchange', exchange)],
                                         states = {
-                                            EXCHANGE: [
+                                            AMOUNT: [
                                                 MessageHandler(filters.TEXT, amount),
                                                 
                                             ],
-                                            AMOUNT: [
+                                            CURRENCY1: [
                                                 MessageHandler(filters.TEXT, currency1),
                                                 
                                             ],
-                                            CURRENCY1: [
+                                            CURRENCY2: [
                                                 MessageHandler(filters.TEXT, currency2),
                                                
                                             ],
-                                            CURRENCY2: [
+                                            EXCHANGE: [
                                                  MessageHandler(filters.TEXT, exchange)
                                             ],
-                                            START: [
-                                                 MessageHandler(filters.TEXT, exchange)
+                                            ERROR: [
+                                                 MessageHandler(filters.TEXT, error)
                                             ]
                                         },
                                         fallbacks= [CommandHandler("exchange", exchange)],
         )
-        application.add_handler(start_handler)
         application.add_handler(exchange_handler)
         application.run_polling()
