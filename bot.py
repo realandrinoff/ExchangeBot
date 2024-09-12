8d93cb24cccf9c0f9cc3704cde492d23bd64f048
Andrey Smirnoff <andrinoff.69@gmail.com>
2024-09-12 13:30:27 +0400

    Bug fix

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 import logging
+from math import isfinite
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, ConversationHandler, MessageHandler, filters
 from rate import convert
 import decimal
+<<<<<<< HEAD
 
 
+=======
+from decimal import Decimal
+>>>>>>> Mac
 
 # This part is responsible for logging so we wouldnt skip code errors
 logging.basicConfig(
@@ -12,15 +17,26 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-# fsfsdf
+# 
 while True:
+<<<<<<< HEAD
     AMOUNT, CURRENCY1, CURRENCY2, EXCHANGE, ERROR = range(5)
+=======
+    # def errorreport(errors, user_id, user_name):
+    #     errorlist = open('errors.txt', "a")
+    #     errorlist.write(user_id, " AKA ", user_name, " encounted an error: ", errors)
+    #     errorlist.close()
+    AMOUNT, CURRENCY1, CURRENCY2, EXCHANGE, ERROR = range(5)
+    
+    
+>>>>>>> Mac
     async def amount(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
             amount1 = update.message.text
             print(amount1)
             context.user_data["amount"] = decimal(amount1)
             try:
+<<<<<<< HEAD
                 await update.message.reply_text('Now write the currency (e.g. usd, gel, eur)')
                 return CURRENCY1
             except ValueError: 
@@ -32,19 +48,53 @@ while True:
     async def currency1(update: Update, context: ContextTypes.DEFAULT_TYPE):
         currency_1 = update.message.text
         context.user_data["currency1"] = currency_1
+=======
+                int(amount1)
+            except ValueError:
+                await update.message.reply_text ('Write the number')
+                return AMOUNT
+            context.user_data["amount"] = Decimal(str(amount1))
+            print (context.user_data["amount"])
+            print (isfinite(context.user_data["amount"]))
+            if isfinite(context.user_data["amount"]) == True:
+                try:
+                    await update.message.reply_text('Now write the currency (e.g. usd, gel, eur)')
+                    return CURRENCY1
+                except ValueError: 
+                    await update.message.reply_text("Error, write just the amount.") 
+                    return ERROR
+            else: 
+                await update.message.reply_text("You can't use infinity, nor NaN")
+                return AMOUNT
+
+    async def currency1(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        currency_1 = update.message.text
+>>>>>>> Mac
         currency_1 = currency_1.strip().upper()
         print(currency_1)
+        context.user_data["currency1"] = currency_1
+
         currency_1 = str(currency_1)
         try:
             if len(currency_1) == 3:
+<<<<<<< HEAD
                 await update.message.reply_text ('Write the currency you want ' + context.user_data["amount"] + context.user_data['currency1'] + ' in')
+=======
+                await update.message.reply_text ('Write the currency you want ' + str(context.user_data["amount"]) + context.user_data['currency1'] + ' in')
+>>>>>>> Mac
                 return CURRENCY2
             else:
                 await update.message.reply_text("Error, write just the currency! (It has to be 3 letters long) (e.g USD, GEL)") 
-        except Exception:
+        except ValueError as e:
+            print(e)
             await update.message.reply_text("Error, try again through /exchange")
+<<<<<<< HEAD
             await exchange(update, context)
         
+=======
+            return ERROR
+
+>>>>>>> Mac
         
     async def currency2(update: Update, context: ContextTypes.DEFAULT_TYPE):
         currency_2 = update.message.text
@@ -54,6 +104,7 @@ while True:
         currency_2 = str(currency_2)
         try:
             if len(currency_2) == 3:
+<<<<<<< HEAD
                     finalresult = convert(context.user_data["amount"], context.user_data["currency1"], context.user_data["currency2"])
                     finalresult = str(finalresult)
                     await update.message.reply_text('Your rate for ' +context.user_data["amount"]+context.user_data["currency1"] + ' = '+ finalresult+ context.user_data["currency1"]+ '.\n\n\n\nThank you for using my service! \nType anything to proceed!\n\nCredits: @andrinoff')
@@ -63,6 +114,17 @@ while True:
         except Exception:
             await update.message.reply_text("Error, try again through /exchange")
             await exchange(update, context)
+=======
+                    finalresult = convert(Decimal(context.user_data["amount"]), str(context.user_data["currency1"]), str(context.user_data["currency2"]))
+                    print(finalresult)
+                    finalresult = str(finalresult)
+                    await update.message.reply_text('Your rate for ' +str(context.user_data["amount"])+context.user_data["currency1"] + ' = '+ str(finalresult)+ context.user_data["currency1"]+ '.\n\n\n\nThank you for using my service! \nType anything to proceed!\n\nCredits: @andrinoff')
+                    return EXCHANGE
+            else:
+                await update.message.reply_text("Error, write just the currency! (It has to be 3 letters long) (e.g USD, GEL)") 
+        except Exception :
+            return ERROR
+>>>>>>> Mac
 
         
     async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -72,6 +134,7 @@ while True:
         context.user_data['currency2']  = None
         return EXCHANGE
     async def exchange(update: Update, context: ContextTypes.DEFAULT_TYPE):
+<<<<<<< HEAD
         amount1 = None
         currency_1 = None
         currency_2 = None
@@ -83,6 +146,20 @@ while True:
 
     if __name__ == '__main__':
         application = ApplicationBuilder().token('7307380567:AAHrnAsxUxwlg7cXWGjFvwlBS_NmoyirJ4I').build()        
+=======
+        try:
+            context.user_data['amount']= None
+            context.user_data['currency1'] = None
+            context.user_data['currency2'] = None
+            await update.message.reply_text('Write AMOUNT (e.g. 20)')         
+            return AMOUNT
+        except:
+            return ERROR
+
+    if __name__ == '__main__':
+        # application = ApplicationBuilder().token('7307380567:AAHrnAsxUxwlg7cXWGjFvwlBS_NmoyirJ4I').build()     
+        application = ApplicationBuilder().token('6993319781:AAGPkkgWZARSMSc94FBIw9vYZ-e09eyTqoM').build()       
+>>>>>>> Mac
         exchange_handler = ConversationHandler(
             entry_points= [CommandHandler('exchange', exchange)],
                                         states = {
