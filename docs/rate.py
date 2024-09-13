242f0b79e72a3f93434ef79d4442ef19a2f74d85
Andrey Smirnoff <andrinoff.69@gmail.com>
2024-09-13 08:50:56 +0400

    SS

--- docs/rate.py
+++ docs/rate.py
@@ -1,30 +1,43 @@
-from currency_converter import CurrencyConverter
 import math
 import currencyapicom
-
+import decimal
+from decimal import getcontext, Decimal
 
 def convert(amount, rate1, rate2):
-    global finalresult
-    amount = int(amount)
-    client = currencyapicom.Client('cur_live_b8rb8eMo1xzDTDdZDHdMEKh0dwy6YfmFYRDTFdHb')
-    result = client.latest(rate1,currencies=[rate2])
-    print(result)
-    print(type(result))
-    val1 = result.get('data')
-    print(val1)
-    val2 = val1.get(rate2)
-    print(val2)
-    val = val2.get("value")
-    print (val)
-    print(type(val))
-    print(val)
-    print(amount)
-    finalresult = val * amount
-    print(finalresult)
-    finalresult = math.ceil(finalresult)
-    print(finalresult)
-    return finalresult
+    try:
+        getcontext().prec = 999
 
+        client = currencyapicom.Client('cur_live_b8rb8eMo1xzDTDdZDHdMEKh0dwy6YfmFYRDTFdHb')
+        result = client.latest(rate1,currencies=[rate2])
+        print(result)
+        print(type(result))
+        val1 = result.get('data')
+        print(val1)
+        val2 = val1.get(rate2)
+        print(val2)
 
+        try:
+            val = val2.get("value")
+            print (val)
+            print(type(val))
+            print(val)
+            val = str(val)
+            print(amount)
+            finalresult = Decimal(val) * Decimal(str(amount))
+            finalresult = round(Decimal(finalresult), 2)
+            int(finalresult)
+            print ('went through 3')
+            print(finalresult)
+            finalresult = str(finalresult)
+            print(finalresult)
+            return finalresult
+        except ValueError:
+            print(ValueError)
+            return False
 
+    except Exception as e:
+        print (e)
+        return False 
+
+# sfdjls;fjdsf
 # convert(100, "RUB", "POP")
